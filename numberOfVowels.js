b622c9906b323a2677265bbeaa646ee565052e72
IshniyazovFail <filimen@mail.ru>
2022-04-16 11:11:06 +0300

    first commit

---
+++ numberOfVowels.js
@@ -0,0 +1,20 @@
+function getCount(str) {
+   let vowelsCount = 0;
+   for (let i=0;i<str.length;i++){
+
+  
+    switch(str[i]){
+        case "a":
+         case "e":
+         case "i":
+         case "o":
+         case "u":
+       vowelsCount ++;
+        break;
+        
+    }
+   }
+    // enter your majic here
+    
+    return vowelsCount;
+  }
